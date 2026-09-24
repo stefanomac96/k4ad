e712b64c1d73999a4de90052a0a33d61f3d28570
stefanomac96 <stefano@stelexsoftware.com>
2026-09-24 20:37:51 +0200

    2026-09-24-B

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Stelex Software-KING 4 A DAY-1.0";
+const contentToCache = [
+    "Build/k4ad.loader.js",
+    "Build/k4ad.framework.js",
+    "Build/k4ad.data",
+    "Build/k4ad.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
